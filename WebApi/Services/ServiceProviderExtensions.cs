d4e81a1f821ea7cd742ec64450f2be2739e055ed
TankistPro <nakol.nayk@mail.ru>
2022-09-11 00:19:10 +0300

    Added ServiceProviderExtensions

---
+++ WebApi/Services/ServiceProviderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using WebApi.Context;
+using WebApi.Interfaces;
+using WebApi.Interfaces.Repositories;
+using WebApi.Interfaces.Services;
+using WebApi.Mapping;
+using WebApi.Models;
+using WebApi.Repositories;
+
+namespace WebApi.Services
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void InitServices(this IServiceCollection services)
+        {
+            services.AddTransient<IBaseRepository<User>, BaseRepository<User>>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IUserRepository, UserRepository>();
+        }
+
+        public static void InitDbContext(this IServiceCollection services)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            services.AddControllers();
+            services.AddAutoMapper(typeof(AppMapping));
+            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -31,20 +31,8 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IBaseRepository<User>, BaseRepository<User>>();
-            services.AddTransient<IUserService, UserService>();
-            services.AddTransient<IUserRepository, UserRepository>();
-
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            string connectionString = config.GetConnectionString("DefaultConnection");
-
-            services.AddControllers();
-            services.AddAutoMapper(typeof(AppMapping));
-            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(connectionString));
+            services.InitServices();
+            services.InitDbContext();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
